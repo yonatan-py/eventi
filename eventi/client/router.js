d66cfd1a26e7031c18d1fb07899692d4aa3b7f5b
yonatan <yonatan.py@gmail.com>
2015-02-26 20:57:02 +0200

    full flow

--- eventi/client/router.js
+++ eventi/client/router.js
@@ -5,14 +5,26 @@ Router.route('/', function () {
 Router.route('/rsvp/:id/:answer', function () {
     console.log(this.params);
     var notificationId = this.params.id;
-    var answer = this.params.answer;
-    answer = answer == "yes" ? true : false;
+    var going = this.params.answer;
+    going = going == "yes" ? true : false;
     var notification = Notifications.findOne({"notification_id": notificationId});
     var eventId = notification.event;
     var userId = notification.user;
     var event = Events.findOne({_id: eventId});
 
-    console.log(event);
-    this.render('answer');
+    for(var user in event.users){
+        if(event.users[user].userId == userId){
+            event.users[user].rsvp = true;
+            event.users[user].going = going;
+            break;
+        }
+    }
+    Events.update(event._id, {$set: {users: event.users}});
 
+    if(going){
+        this.render('yes');
+    }
+    else{
+        this.render('no');
+    }
 });
---
+++ eventi/lib/models.js
@@ -0,0 +1,2 @@
+Events = new Meteor.Collection("events");
+Notifications = new Meteor.Collection("notifications");
